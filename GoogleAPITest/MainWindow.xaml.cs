f9848da8483d1ee1d342016168d69a9da51824f8
chris84948 <chris84948@gmail.com>
2016-01-10 22:58:58 -0600

    SyncUpdate

--- GoogleAPITest/MainWindow.xaml.cs
+++ GoogleAPITest/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using Google;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Download;
 using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,13 @@ namespace GoogleAPITest
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// This permission only allows viewing of files I have created or opened with this app
+        /// </summary>
         private string[] Scopes = { DriveService.Scope.DriveFile };
         private string AppName = "GoogleAPITest";
         private UserCredential credentials;
+        private DriveService service;
         
         public MainWindow()
         {
@@ -31,22 +37,36 @@ namespace GoogleAPITest
             string credPath = AppDomain.CurrentDomain.BaseDirectory + AppName;
 
             credentials = GetCredentials(credPath);
-        }
 
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = AppName,
+            });
+        }
+                
         private UserCredential GetCredentials(string credPath)
         {
             try
             {
+                UserCredential userCredential;
                 using (var stream = new System.IO.FileStream("client_secret.json",
                                                              System.IO.FileMode.Open,
                                                              System.IO.FileAccess.Read))
                 {
-                    return GoogleWebAuthorizationBroker.AuthorizeAsync(stream,
-                                                                       Scopes,
-                                                                       "user",
-                                                                       CancellationToken.None,
-                                                                       new FileDataStore(credPath, true)).Result;
+                    userCredential = GoogleWebAuthorizationBroker.AuthorizeAsync(stream,
+                                                                                 Scopes,
+                                                                                 "user",
+                                                                                 CancellationToken.None,
+                                                                                 new FileDataStore(credPath, true)).Result;
                 }
+
+                if (userCredential.IsExpired())
+                {
+                    bool b = userCredential.RefreshTokenAsync(CancellationToken.None).Result;
+                }
+
+                return userCredential;
             }
             catch (AggregateException ae)
             {
@@ -57,31 +77,224 @@ namespace GoogleAPITest
             return null;
         }
 
-        private void CreateFile()
+        public static IList<File> GetFiles(DriveService service, string search)
         {
-            var driveService = new DriveService();
+            try
+            {
+                //List all of the files and directories for the current user.  
+                // Documentation: https://developers.google.com/drive/v2/reference/files/list
+                FilesResource.ListRequest list = service.Files.List();
 
-            // Upload a new file
+                FilesResource.ListRequest request = service.Files.List();
+                FileList files = request.Execute();
+
+                return files.Files;
+            }
+            catch (Exception ex)
+            {
+                // In the event there is an error with the request.
+                Console.WriteLine(ex.Message);
+            }
+            return new List<File>();
+        }
+
+        /// <summary>
+        /// Create a new Directory.
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
+        /// </summary>
+        /// <param name="service">a Valid authenticated DriveService</param>
+        /// <param name="title">The title of the file. Used to identify file or folder name.</param>
+        /// <param name="description">A short description of the file.</param>
+        /// <param name="parent"></param>
+        /// <returns>
+        /// Collection of parent folders which contain this file. 
+        /// Setting this field will put the file in all of the provided folders. root folder.
+        /// </returns>
+        public static File CreateDirectory(DriveService service, string title, string description, string parent = "root")
+        {
+            File NewDirectory = null;
+
+            // Create metaData for a new Directory
             File body = new File();
-            body.Name = "Test1.txt";
-            body.Description = "Test File";
-            body.MimeType = "text/plain";
-            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(TextBox.Text);
-            var stream = new System.IO.MemoryStream(byteArray);
-            FilesResource.CreateMediaUpload request = driveService.Files.Create(body, stream, "text/plain");
-            request.Upload();
-            File file = request.ResponseBody;
+            body.Name = title;
+            body.Description = description;
+            body.MimeType = "application/vnd.google-apps.folder";
+            body.Parents = new List<string>() { parent };
 
-            // Show all files
-            var list2 = driveService.Files.List().Execute();
-            if (list2.Files != null)
-                foreach (var fileItem in list2.Files)
-                    Console.WriteLine(fileItem.Name + " - " + fileItem.Description);
+            try
+            {
+                FilesResource.CreateRequest request = service.Files.Create(body);
+                NewDirectory = request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return NewDirectory;
+        }
+
+        /// <summary>
+        /// Uploads a file
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
+        /// </summary>
+        /// <param name="service">a Valid authenticated DriveService</param>
+        /// <param name="uploadFile">path to the file to upload</param>
+        /// <param name="parent">
+        /// Collection of parent folders which contain this file. 
+        /// Setting this field will put the file in all of the provided folders. root folder.
+        /// </param>
+        /// <returns>
+        /// If upload succeeded returns the File resource of the uploaded file 
+        /// If the upload fails returns null
+        /// </returns>
+        public static File UploadFile(DriveService service, string uploadFile, string content, string parent)
+        {
+            File body = new File();
+            body.Name = System.IO.Path.GetFileName(uploadFile);
+            body.Description = "Test File 1";
+            body.Parents = new List<string>() { parent };
+
+            // File's content.
+            var stream = new System.IO.MemoryStream(Encoding.UTF8.GetBytes(content ?? ""));
+            try
+            {
+                FilesResource.CreateMediaUpload request = service.Files.Create(body, stream, String.Empty);
+                request.Upload();
+                return request.ResponseBody;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Updates a file
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/update
+        /// </summary>
+        /// <param name="service">a Valid authenticated DriveService</param>
+        /// <param name="uploadFile">path to the file to upload</param>
+        /// <param name="parent">
+        /// Collection of parent folders which contain this file. 
+        /// Setting this field will put the file in all of the provided folders. root folder.
+        /// </param>
+        /// <param name="fileId">the resource id for the file we would like to update</param>
+        /// <returns>
+        /// If upload succeeded returns the File resource of the uploaded file 
+        /// If the upload fails returns null
+        /// </returns>
+        public static File UpdateFile(DriveService service, string uploadFile, string parent, string fileId)
+        {
+            File body = new File();
+            body.Name = System.IO.Path.GetFileName(uploadFile);
+            body.Parents = new List<string>() { parent };
+
+            // File's content.
+            byte[] byteArray = System.IO.File.ReadAllBytes(uploadFile);
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+            try
+            {
+                FilesResource.UpdateMediaUpload request = service.Files.Update(body, fileId, stream, String.Empty);
+                request.Upload();
+                return request.ResponseBody;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                return null;
+            }
+        }
+
+        public static File TrashFile(DriveService service, string uploadFile,  string fileId)
+        {
+            File body = new File();
+            body.Trashed = true;
+
+            // File's content.
+            byte[] byteArray = System.IO.File.ReadAllBytes(uploadFile);
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+            try
+            {
+                FilesResource.UpdateRequest request = service.Files.Update(body, fileId);
+                return request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                return null;
+            }
+        }
+
+        public static File UntrashFile(DriveService service, string uploadFile, string fileId)
+        {
+            File body = new File();
+            body.Trashed = false;
+
+            // File's content.
+            byte[] byteArray = System.IO.File.ReadAllBytes(uploadFile);
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+            try
+            {
+                FilesResource.UpdateRequest request = service.Files.Update(body, fileId);
+                return request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Download a file
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/get
+        /// </summary>
+        /// <param name="service">a Valid authenticated DriveService</param>
+        /// <param name="fileResource">File resource of the file to download</param>
+        /// <param name="saveTo">location of where to save the file including the file name to save it as.</param>
+        /// <returns></returns>
+        public static void DownloadFile(DriveService service, File fileResource, string saveTo)
+        {
+            var fileId = fileResource.Id;
+            var request = service.Files.Get(fileId);
+            var stream = new System.IO.MemoryStream();
+
+            // Add a handler which will be notified on progress changes.
+            // It will notify on each chunk download and when the
+            // download is completed or failed.
+            request.MediaDownloader.ProgressChanged +=
+                (IDownloadProgress progress) =>
+                {
+                    switch (progress.Status)
+                    {
+                        case DownloadStatus.Downloading:
+                            {
+                                Console.WriteLine(progress.BytesDownloaded);
+                                break;
+                            }
+                        case DownloadStatus.Completed:
+                            {
+                                Console.WriteLine("Download complete.");
+                                break;
+                            }
+                        case DownloadStatus.Failed:
+                            {
+                                Console.WriteLine("Download failed.");
+                                break;
+                            }
+                    }
+                };
+            request.Download(stream);
         }
 
         private void btnUpload_Click(object sender, RoutedEventArgs e)
         {
-            CreateFile();
+            var directory = CreateDirectory(service, "DriveAPITest", "Drive API Test Application");
+            var file = UploadFile(service, "File1.txt", TextBox.Text, directory.Id);
+
+            var allFiles = GetFiles(service, "");
         }
 
     }
---
+++ GoogleAPITest/UserCredentialExtension.cs
@@ -0,0 +1,18 @@
+﻿using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2.Responses;
+
+namespace GoogleAPITest
+{
+    public static class UserCredentialExtension
+    {
+        public static bool IsExpired(this UserCredential userCredential)
+        {
+            return DateTime.Now >= userCredential.Token.Issued.AddSeconds((double)userCredential.Token.ExpiresInSeconds);
+        }
+    }
+}
